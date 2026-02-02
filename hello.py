c75831f14ab984d67789a5cebaa535a31c15483d
root <root@5ce7f90a1aab>
2026-02-02 16:15:49 +0000

    Add hello.py: simple script that prints "Hello from megaschool" on startup

---
+++ hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Simple entry point script that prints a greeting.
+
+Running this script will output:
+    Hello from megaschool
+"""
+
+
+def main():
+    print("Hello from megaschool")
+
+
+if __name__ == "__main__":
+    main()
